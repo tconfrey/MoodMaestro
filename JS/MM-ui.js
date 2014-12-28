0fae9550f4540ee7efc7c86536ca478e13aee197
Tony Confrey <tconfrey@gmail.com>
2014-12-28 16:28:40 -0500

    integrated FB sdk, created parse static site, need to run from internet to test FB

--- JS/MM-ui.js
+++ JS/MM-ui.js
@@ -5,7 +5,28 @@ var Post;
 jQuery( document ).on( "pageinit", "#mainpage", function( event ) {
 	Parse.initialize("CKUBetgoCV0iygTQEJaOMpVt5raxZFS61ESh7e4e", "dhILThcP5vWwn0e5tlyIJYpan0EM0ZDzEK3ClV5a");
 
-	Post = Parse.Object.extend("Post");	
+	window.fbAsyncInit = function() {
+		Parse.FacebookUtils.init({ // this line replaces FB.init({
+			appId      : '320082134852887', // Facebook App ID
+			status     : true,  // check Facebook Login status
+			cookie     : true,  // enable cookies to allow Parse to access the session
+			xfbml      : true,  // initialize Facebook social plugins on the page
+			version    : 'v2.2' // point to the latest Facebook Graph API version
+		});
+		
+		// Run code after the Facebook SDK is loaded.
+		login();
+	};
+	
+	(function(d, s, id){
+		var js, fjs = d.getElementsByTagName(s)[0];
+		if (d.getElementById(id)) {return;}
+		js = d.createElement(s); js.id = id;
+		js.src = "http://connect.facebook.net/en_US/sdk.js";
+		fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+
+	Post = Parse.Object.extend("Post");
 });
 
 // load history on list page
@@ -14,19 +35,21 @@ jQuery( document ).on( "pageshow", "#listpage", function (event ) {
 	query.find({
 		success: function(results) {
 			// Do something with the returned Parse.Object values
+			$( "#moods .mooddata" ).remove();
 			for (var i = 0; i < results.length; i++) { 
 				var object = results[i];
 				var d = object.createdAt;
 				var month = d.getMonth()+1;
-				var day = d.getDay();
+				var day = d.getDate();
 				var year = d.getFullYear();
 				var hour = d.getHours();
 				var mins = d.getMinutes();
 				var date = hour+":"+mins+" on "+month+"/"+day+"/"+year;
 				var mood = object.get("mood");
 				var reason = object.get("text");
-				$('#moods tr:last').after('<tr><td>' + date + '</td><td>' + mood + '</td><td>' + reason + '</td></tr>');
+				$('#moods tr:last').after("<tr class='mooddata'><td>" + date + "</td><td>" + mood + "</td><td>" + reason + "</td></tr>");
 			}
+			//$( "#moods" ).trigger( "create" );
 		},
 		error: function(error) {
 			alert("Error: " + error.code + " " + error.message);
@@ -60,4 +83,19 @@ function postit() {
 		}
 	});
 
+}
+
+function login() {
+	Parse.FacebookUtils.logIn(null, {
+		success: function(user) {
+			if (!user.existed()) {
+				alert("User signed up and logged in through Facebook!");
+			} else {
+				alert("User logged in through Facebook!");
+			}
+		},
+		error: function(user, error) {
+			alert("User cancelled the Facebook login or did not fully authorize.");
+		}
+	});
 }
